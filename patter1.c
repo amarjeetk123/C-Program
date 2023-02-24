80c2e42b0ed99ea52e2b1d75751749cfdb9db1b6
amarjeetk123 <ak123del@gmail.com>
2023-02-24 18:55:15 +0530

    addinga new code

---
+++ patter1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{   
+    int row,col,n;
+    printf("give a number");
+    scanf("%d",&n);
+    row=1;
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=row;col++)
+      {  
+          printf("*");
+      }
+      
+        printf("\n");
+    }
+    
+    return 0;
+}
