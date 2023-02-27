f6fe3e43b8cb3986ea8dac78d42166e91378d12c
amarjeetk123 <ak123del@gmail.com>
2023-02-27 17:44:59 +0530

    adding a program of counting the length of a number

---
+++ counting.number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, count, i;
+    printf("enter any number");
+    scanf("%d", &n);
+    if (n == 0)
+    {
+        count = 1;
+    }
+    count = 0;
+    while (n != 0)
+    {
+        n = (n / 10);
+        count++;
+    }
+    printf("%d", count);
+}
