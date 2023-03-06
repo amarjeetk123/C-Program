6aa076fd04666653fafe11bb1d10e11403191e43
amarjeetk123 <ak123del@gmail.com>
2023-03-06 12:30:58 +0530

    adding code of tringle of number

---
+++ triangle/tringleofstar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,row,x,col;
+    printf("enter no of rows");
+    scanf("%d",&n);
+    x=1;
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=row;col++)
+        {
+          printf("%d",x);
+            x=x+1;
+            
+        }   
+        printf("\n");
+        
+      
+    }
+
+    return 0;
+}
