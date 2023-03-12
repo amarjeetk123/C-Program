8a1c8d813033a0d0f32c9fc8bd803aaf8a6f0222
amarjeetk123 <ak123del@gmail.com>
2023-03-12 13:22:06 +0530

    doing some modification

--- pattern2.c
+++ pattern2.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 int main()
-{   
-    int row,col,n;
+{
+    int row, col, n;
     printf("give a number");
-    scanf("%d",&n);
-    row=1;
-    for(row=1;row<=n;row++)
+    scanf("%d", &n);
+    row = 1;
+    for (row = 1; row <= n; row++)
     {
-        for(col=1;col<=row;col++)
-      {  
-          printf("*");
-      }
-      
+        for (col = 1; col <= row; col++)
+        {
+            printf("*");
+        }
+
         printf("\n");
     }
-    
     return 0;
 }
--- special pattern/patter2.c
+++ special pattern/patter2.c
@@ -2,25 +2,22 @@
 
 int main()
 {
-    int n,row,star,space,x;
-    printf("enter no");
-    scanf("%d",&n);
-    
-    x=2*n-1;
-    for(row=1;row<=n;row++)
-    {
-    for(star=1;star<=row;star++)
+  int n, row, star, space, x;
+  printf("enter no");
+  scanf("%d", &n);
+
+  x = 2 * n - 1;
+  for (row = 1; row <= n; row++)
+  {
+    for (star = 1; star <= row; star++)
       printf("*");
-    for(space=1;space<=x;space++)
+    for (space = 1; space <= x; space++)
       printf(" ");
-      x=x-2;
-      for(star=1;star<=row;star++)
-    printf("*");
-    
-      printf("\n");
-    }
+    x = x - 2;
+    for (star = 1; star <= row; star++)
+      printf("*");
 
-
-    return 0;
+    printf("\n");
+  }
+  return 0;
 }
-
